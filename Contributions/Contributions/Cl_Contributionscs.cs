f2c5ecc533f2e73b13f912dbf9ae44112328208d
24paketik <deniska-smirnov-05@mail.ru>
2022-04-28 12:49:48 +0300

    Create class contribution

--- Contributions/Contributions/Cl_Contributionscs.cs
+++ Contributions/Contributions/Cl_Contributionscs.cs
@@ -8,5 +8,24 @@ namespace Contributions
 {
     public class Cl_Contributionscs
     {
+        public int balance; // баланс
+        public string DateOpen; // Дата открытия
+
+        public void OpenContr()
+        {
+            Console.Write("Введие баланс счёта: ");
+            balance = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите  дату открытия счёта:");
+            DateOpen = Console.ReadLine();
+        }
+        public void CloseContr()
+        {
+            balance = 0;
+            DateOpen = "";
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine("Ваш баланс: "+balance);
+        }
     }
 }
