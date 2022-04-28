77785b65b220d67d56279bf097cd77e56648236b
24paketik <deniska-smirnov-05@mail.ru>
2022-04-28 13:06:03 +0300

    Create Cl_Multi.cs

---
+++ Contributions/Contributions/Cl_Multi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contributions
+{
+    internal class Cl_Multi:Cl_Contributionscs
+    {
+        public void takeContr()
+        {
+            Console.Write("ВВедите сумму для снятия со счёта:");
+            balance -= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Деньги успешно сняты");
+        }
+        public void putContr()
+        {
+            Console.Write("ВВедите сумму для внесение на счёт:");
+            balance += Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Деньги успешно внесены");
+        }
+    }
+}
